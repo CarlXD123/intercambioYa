278286e4f804e1c050ca6e763c2dd3118fe2d8d7
CarlXD123 <64506247+CarlXD123@users.noreply.github.com>
2023-07-06 10:57:40 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import ProductDetail from "./products/detail/ProductDetail";
 import { Routes, Route } from "react-router-dom";
 import ProductList from "./products/ProductList";
 import Landing from "./landing/Landing";
+import Subasta from "./subasta/Subasta";
 import Logueo from "./login/Logueo";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Logueo />} />
         <Route path="/product" element={<ProductList />} />
+        <Route path="/subasta" element={<Subasta />} />
         <Route path="/products/:slug" element={<ProductDetail />} />
         <Route path="/inicio" element={<Landing />} />
       </Routes>
