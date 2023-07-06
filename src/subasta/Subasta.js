5b3ce4689a904f237cc8ada473987df18c2d2069
CarlXD123 <64506247+CarlXD123@users.noreply.github.com>
2023-07-06 10:55:28 -0500

    Add files via upload

---
+++ src/subasta/Subasta.js
@@ -0,0 +1,214 @@
+import { Link } from "react-router-dom";
+import ProductSub from "../products/ProductSub";
+import ProductH from "../products/ProductH";
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import ScrollToTopOnMount from "../template/ScrollToTopOnMount";
+import Header from "../template/Header";
+
+const categories = [
+  "All Products",
+  "Celulares & Tablets",
+  "Laptops",
+  "Vehiculos",
+  "Decoracion",
+  "Articulos de Belleza",
+];
+
+const manufacturers = ["Accesorios", "Tecnologia", "Vestimenta", "Juegos", "Productos de belleza", "Articulos de cocina"];
+
+function FilterMenuLeft() {
+  return (
+    <ul className="list-group list-group-flush rounded">
+      <li className="list-group-item">
+        <h5 className="mt-1 mb-1">Categorias</h5>
+        <div className="d-flex flex-column">
+          {manufacturers.map((v, i) => {
+            return (
+              <div key={i} className="form-check">
+                <input className="form-check-input" type="checkbox" />
+                <label className="form-check-label" htmlFor="flexCheckDefault">
+                  {v}
+                </label>
+              </div>
+            );
+          })}
+        </div>
+      </li>
+    
+    </ul>
+  );
+}
+
+function Subasta() {
+  const [viewType, setViewType] = useState({ grid: true });
+
+  function changeViewType() {
+    setViewType({
+      grid: !viewType.grid,
+    });
+  }
+
+  return (
+    <div className="container mt-5 py-4 px-xl-5">
+      <Header />
+      <ScrollToTopOnMount />
+      <nav aria-label="breadcrumb" className="bg-custom-light rounded">
+        <ol className="breadcrumb p-3 mb-0">
+          <li className="breadcrumb-item">
+            <Link
+              className="text-decoration-none link-secondary"
+              to="/products"
+              replace
+            >
+              Subastas disponibles
+            </Link>
+          </li>
+         
+        </ol>
+      </nav>
+      <div className="h-scroller d-block d-lg-none">
+        <nav className="nav h-underline">
+          {categories.map((v, i) => {
+            return (
+              <div key={i} className="h-link me-2">
+                <Link
+                  to="/subasta"
+                  className="btn btn-sm btn-outline-dark rounded-pill"
+                  replace
+                >
+                  {v}
+                </Link>
+              </div>
+            );
+          })}
+        </nav>
+      </div>
+
+      <div className="row mb-3 d-block d-lg-none">
+        <div className="col-12">
+          <div id="accordionFilter" className="accordion shadow-sm">
+            <div className="accordion-item">
+              <h2 className="accordion-header" id="headingOne">
+                <button
+                  className="accordion-button fw-bold collapsed"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#collapseFilter"
+                  aria-expanded="false"
+                  aria-controls="collapseFilter"
+                >
+                  Filter Products
+                </button>
+              </h2>
+            </div>
+            <div
+              id="collapseFilter"
+              className="accordion-collapse collapse"
+              data-bs-parent="#accordionFilter"
+            >
+              <div className="accordion-body p-0">
+                <FilterMenuLeft />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="row mb-4 mt-lg-3">
+        <div className="d-none d-lg-block col-lg-3">
+          <div className="border rounded shadow-sm">
+            <FilterMenuLeft />
+          </div>
+        </div>
+        <div className="col-lg-9">
+          <div className="d-flex flex-column h-100">
+            <div className="row mb-3">
+              <div className="col-lg-3 d-none d-lg-block">
+                <select
+                  className="form-select"
+                  aria-label="Default select example"
+                  defaultValue=""
+                >
+                  <option value="">Todas las marcas</option>
+                  <option value="1">Legion</option>
+                  <option value="2">Asus</option>
+                  <option value="3">Lenovo</option>
+                  <option value="4">Pantene</option>
+                  <option value="5">Chanel</option>
+                  <option value="6">Adidas</option>
+                  <option value="7">Sony</option>
+                  <option value="8">Nintendo</option>
+                </select>
+              </div>
+              <div className="col-lg-9 col-xl-5 offset-xl-4 d-flex flex-row">
+                <div className="input-group">
+                  <input
+                    className="form-control"
+                    type="text"
+                    placeholder="Search products..."
+                    aria-label="search input"
+                  />
+                  <button className="btn btn-outline-dark">
+                    <FontAwesomeIcon icon={["fas", "search"]} />
+                  </button>
+                </div>
+                <button
+                  className="btn btn-outline-dark ms-2 d-none d-lg-inline"
+                  onClick={changeViewType}
+                >
+                  <FontAwesomeIcon
+                    icon={["fas", viewType.grid ? "th-list" : "th-large"]}
+                  />
+                </button>
+              </div>
+            </div>
+            <div
+              className={
+                "row row-cols-1 row-cols-md-2 row-cols-lg-2 g-3 mb-4 flex-shrink-0 " +
+                (viewType.grid ? "row-cols-xl-3" : "row-cols-xl-2")
+              }
+            >
+              {Array.from({ length: 1 }, (_, i) => {
+                if (viewType.grid) {
+                  return (
+                    <ProductSub key={i} percentOff={i % 2 === 0 ? 15 : null} />
+                  );
+                }
+                return (
+                  <ProductH key={i} percentOff={i % 4 === 0 ? 15 : null} />
+                );
+              })}
+            </div>
+            <div className="d-flex align-items-center mt-auto">
+              <span className="text-muted small d-none d-md-inline">
+                Showing 10 of 100
+              </span>
+              <nav aria-label="Page navigation example" className="ms-auto">
+                <ul className="pagination my-0">
+                  <li className="page-item">
+                    <button className="page-link">Previous</button>
+                  </li>
+                  <li className="page-item">
+                    <button className="page-link">1</button>
+                  </li>
+                  <li className="page-item active">
+                    <button className="page-link">2</button>
+                  </li>
+                  <li className="page-item">
+                    <button className="page-link">3</button>
+                  </li>
+                  <li className="page-item">
+                    <button className="page-link">Next</button>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Subasta;
