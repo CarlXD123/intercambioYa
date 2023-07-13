2d32b708e98f57bb81711e8c076d0bc029a607b9
CarlXD123 <64506247+CarlXD123@users.noreply.github.com>
2023-07-13 13:34:21 -0500

    Update ProductList.js

--- src/products/ProductList.js
+++ src/products/ProductList.js
@@ -35,30 +35,6 @@ function FilterMenuLeft() {
           })}
         </div>
       </li>
-      <li className="list-group-item">
-        <h5 className="mt-1 mb-2">Price Range</h5>
-        <div className="d-grid d-block mb-3">
-          <div className="form-floating mb-2">
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Min"
-              defaultValue="100"
-            />
-            <label htmlFor="floatingInput">Min Price</label>
-          </div>
-          <div className="form-floating mb-2">
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Max"
-              defaultValue="500"
-            />
-            <label htmlFor="floatingInput">Max Price</label>
-          </div>
-          <button className="btn btn-dark">Apply</button>
-        </div>
-      </li>
     </ul>
   );
 }
